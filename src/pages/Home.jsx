807898a3eecdb1e63188b139ad37f73fbfc9a783
razieh i <izanloo@amwajco.net>
2025-10-22 10:03:48 +0330

    feat: create taskForm component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { TaskForm } from "../components/taskForm";
+
+export default function Home() {
+  return (
+    <>
+      <h1> Task Management Application</h1>
+      <TaskForm />
+    </>
+  );
+}
